690232b1aa981a9b4ba6bd0d8838264c8a118a3c
Ar-Akhil <ar.akhil1511@gmail.com>
2023-11-16 13:17:34 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import booksroutes from "./routes/booksRoutes.js";
 import cors from "cors";
-import { mongoDBURL } from "./config.js";
+import mongoDBURL from "./config.js";
 
 const app = express();
-dontenv.config({ path: "./.env" });
 
 //middleware for parsing request body
 app.use(
